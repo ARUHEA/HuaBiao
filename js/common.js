cf25ea6c04a30096f268f241d838903af5f53eef
ARUHEA <1091347757@qq.com>
2019-12-20 19:41:31 +0800

    12.20第二次提交代码

--- js/common.js
+++ js/common.js
@@ -2,3 +2,22 @@ $(".top-link ul li a").bind('click', function () {
     $(".top-link ul li a").removeClass("a-bottom");
     $(this).addClass("a-bottom");
 });
+
+$("#btn-about").click(
+    function () {
+        $(this).addClass("selected");
+        $("#btn-teame").removeClass("selected");
+        $("#txt-about").removeClass("txtnone");
+        $("#txt-teame").addClass("txtnone");
+    }
+
+)
+
+$("#btn-teame").click(
+    function () {
+        $(this).addClass("selected");
+        $("#btn-about").removeClass("selected");
+        $("#txt-teame").removeClass("txtnone");
+        $("#txt-about").addClass("txtnone");
+    }
+)
