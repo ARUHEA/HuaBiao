25c3b17c25a319064927e19fc73246a8dbfc026b
ARUHEA <1091347757@qq.com>
2019-12-26 19:10:33 +0800

    12.26优化部分页面

--- js/phone.js
+++ js/phone.js
@@ -1,16 +1,19 @@
 
 $(".ph-menulist").hide();
 $("#menu").click(function () {
+    $(".ph-menulist ul li").css("background-color","rgba(0,0,0,0.5)").css("border-bottom","none")
     $("#t-none").show();
     if ($("#menu").next("div").is(":hidden")) {
-        $(".ph-menulist").show();
+        $(".ph-menulist").fadeIn();
     } else {
-        $(".ph-menulist").hide();
+        $(".ph-menulist").fadeOut();
     }
+
+
 })
 
 $('#t-none').on('touchstart',function(){
-    $(".ph-menulist").hide();
+    $(".ph-menulist").fadeOut();
     $("#t-none").hide();
 })
 
